536ccc17663aeade8ea77719532550dce066e565
Anchal-ka <Anchalpreetkaur1@dcmail.ca>
2025-01-24 13:45:34 -0500

    Added the constants, and made the get_user_input function, which takes the prompt, min value, and the max value. If the user inputs anything else than an integer, it asks for the input again. Also value needs to be in the range of the min and max value, If not it asks for the input again.

--- Lab1_Group12/src/lab1_sanyam_anchal.py
+++ Lab1_Group12/src/lab1_sanyam_anchal.py
@@ -6,10 +6,23 @@
 import random
 import string
 
-# Function to get user input (skeleton)
+PASSWORD_MIN_LENGTH = 8
+PASSWORD_MAX_LENGTH = 100
+MIN_LETTERS_LENGTH = 2
+MIN_DIGITS_LENGTH = 1
+MIN_SPECIAL_CHARACTERS_LENGTH = 1
+
+# Function to get user input
 def get_user_input(prompt, min_value, max_value):
-    # Implement logic to get valid user input within a range
-    pass
+    while True:
+        try:
+            user_input = int(input(f"{prompt}: "))
+            if min_value <= user_input <= max_value:
+                return user_input
+            else:
+                print(f"Please enter a value between {min_value} and {max_value}, inclusive, to create a secure password.")
+        except ValueError:
+            print("The input needs to be an integer.")
 
 # Function to generate a password (skeleton)
 def generate_password(length, num_letters, num_digits, num_specials):
